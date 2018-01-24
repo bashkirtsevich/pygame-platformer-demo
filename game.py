9a70f95627ec30d8fc285f9b0c01f95330c5d97a
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2018-01-24 23:16:20 +0500

    Simplify code

--- game.py
+++ game.py
@@ -37,10 +37,8 @@ def camera_configure(camera, target_rect):
     _, _, w, h = camera
     l, t = -l + WIN_WIDTH / 2, -t + WIN_HEIGHT / 2
 
-    l = min(0, l)  # Не движемся дальше левой границы
-    l = max(-(camera.width - WIN_WIDTH), l)  # Не движемся дальше правой границы
-    t = max(-(camera.height - WIN_HEIGHT), t)  # Не движемся дальше нижней границы
-    t = min(0, t)  # Не движемся дальше верхней границы
+    l = max(-(camera.width - WIN_WIDTH), min(0, l))
+    t = min(0, max(-(camera.height - WIN_HEIGHT), t))
 
     return Rect(l, t, w, h)
 
